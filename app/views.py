decb371de8a6dc630c05b5807c7e0f51138e67cc
biagmss <ars.rp9@gmail.com>
2026-03-19 17:34:11 -0300

    Botão quero doar,botão selecionar doação de roupa e páginas(bia)

--- app/views.py
+++ app/views.py
@@ -2,3 +2,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+def cadastro_roupa(request):
+    return render(request, 'cadastro_roupa.html')
+def selecao_tipo_doacao(request):
+    return render(request, 'selecao_tipo_doacao.html')
+def cadastro_roupa(request):
+    return render(request, 'cadastro_roupa.html')
--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from app.views import home 
-
+from app.views import home ,cadastro_roupa,selecao_tipo_doacao
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'),
-    # Verifique se o browser_reload está no settings.py, se sim, mantenha:
-    path("__reload__/", include("django_browser_reload.urls")), 
+    path('', home, name='home'), 
+    path("__reload__/", include("django_browser_reload.urls")),
+    path('cadastrar-roupa/', cadastro_roupa, name='cadastro_roupa'),
+    path('selecao-tipo-doacao/', selecao_tipo_doacao, name='selecao_tipo_doacao'),
 ]
