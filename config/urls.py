1491234b703edae87e2c1c4fc9e2675092fb14aa
biagmss <ars.rp9@gmail.com>
2026-03-19 14:43:13 -0300

    Versão 1 do Projeto 2 (bia)

---
+++ config/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path, include
+from app.views import home 
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', home, name='home'),
+    # Verifique se o browser_reload está no settings.py, se sim, mantenha:
+    path("__reload__/", include("django_browser_reload.urls")), 
+]
