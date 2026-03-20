e7cacc5f147d946edfcfdd57c9777f877605a859
biagmss <ars.rp9@gmail.com>
2026-03-20 11:34:21 -0300

    imagens e organização visual(bia)

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', home, name='home'), 
     path("__reload__/", include("django_browser_reload.urls")),
     path('cadastrar-roupa/', cadastro_roupa, name='cadastro_roupa'),
-    path('selecao-tipo-doacao/', selecao_tipo_doacao, name='selecao_tipo'),
+    path('selecao-tipo-doacao/', selecao_tipo_doacao, name='selecao_tipo_doacao'),
     path('login/', login_view, name='login'),
     path('cadastro-usuario/', cadastro_usuario, name='cadastro_usuario'),
     path('cadastro-alimento/', cadastro_alimento, name='cadastro_alimento'),
