e50444aec596c5542fe2e180ab3dd19a87c57cd0
mahdiias <marceladiias15@gmail.com>
2026-03-25 13:40:21 -0300

    simulação de cadastro de doação, alterações de todas as telas (design e logica)

--- app/views.py
+++ app/views.py
@@ -1,14 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+doacoes_lista = []
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'home.html', {
+        'doacoes': doacoes_lista
+    })
+
 def cadastro_roupa(request):
+    if request.method == 'POST':
+        doacoes_lista.append({
+            'nome': request.POST.get('nome'),
+            'quantidade': request.POST.get('quantidade'),
+            'localizacao': request.POST.get('localizacao'),
+            'tipo': 'Roupa'
+        })
+        return redirect('home')
+
     return render(request, 'cadastro_roupa.html')
+
+def cadastro_alimento(request):
+    if request.method == 'POST':
+        doacoes_lista.append({
+            'nome': request.POST.get('nome'),
+            'quantidade': request.POST.get('quantidade'),
+            'localizacao': request.POST.get('localizacao'),
+            'tipo': 'Alimento'
+        })
+        return redirect('home')
+
+    return render(request, 'cadastro_alimento.html')
+
 def selecao_tipo_doacao(request):
     return render(request, 'selecao_tipo_doacao.html')
+
 def login_view(request):
+    if request.method == 'POST':
+        request.session['usuario'] = request.POST.get('email')
+        return redirect('home')
+
     return render(request, 'login.html')
+
 def cadastro_usuario(request):
+    if request.method == 'POST':
+        return redirect('login')
+
     return render(request, 'cadastro_usuario.html')
-def cadastro_alimento(request):
-    return render(request, 'cadastro_alimento.html')
--- config/urls.py
+++ config/urls.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
-from app.views import cadastro_alimento, cadastro_usuario, home ,cadastro_roupa,selecao_tipo_doacao,login_view
+from app.views import (
+    home,
+    cadastro_roupa,
+    cadastro_alimento,
+    cadastro_usuario,
+    login_view,
+    selecao_tipo_doacao
+)
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home, name='home'), 
+    path('', home, name='home'),
     path("__reload__/", include("django_browser_reload.urls")),
     path('cadastrar-roupa/', cadastro_roupa, name='cadastro_roupa'),
+    path('cadastro-alimento/', cadastro_alimento, name='cadastro_alimento'),
     path('selecao-tipo-doacao/', selecao_tipo_doacao, name='selecao_tipo_doacao'),
     path('login/', login_view, name='login'),
     path('cadastro-usuario/', cadastro_usuario, name='cadastro_usuario'),
-    path('cadastro-alimento/', cadastro_alimento, name='cadastro_alimento'),
 ]
